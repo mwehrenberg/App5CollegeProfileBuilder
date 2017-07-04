6f524ee4c1b7899e590c22aca71f8a9fffe03252
John Wehrenberg <John@johns-macbook-air.local>
2017-07-04 08:59:49 -0500

    Connected UIObjects to storyboard

--- App 5 - CollegeProfileBuilder/DetailViewController.swift
+++ App 5 - CollegeProfileBuilder/DetailViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var enrollmentTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: College? {
+        didSet {
+            configureView()
         }
     }
 
@@ -33,13 +33,27 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let college = self.detailItem {
+            if nameTextField != nil {
+                nameTextField.text = college.name
+                locationTextField.text = college.location
+                enrollmentTextField.text = String(college.enrollment)
+                imageView.image = UIImage(data : college.image)
+                
+            }
         }
     }
-
-
+    
+    @IBAction func onTappedSaveButton(_ sender: Any) {
+        if let college = self.detailItem {
+            college.name = nameTextField.text!
+            college.location = locationTextField.text!
+            college.enrollment = Double(enrollmentTextField.text!)!
+            college.image = UIImagePNGRepresentation(imageView.image!)!
+        }
+    }
+    
 }
 
